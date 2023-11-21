c26661a11ee7c2b44e3b67e742d27e7fa4d1f0e9
nishithagithub <116818897+nishithagithub@users.noreply.github.com>
2023-11-22 00:08:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,93 +1,89 @@
+let userForm = document.getElementById("user-form");
+
+const retrieveEntries = () => {
+    let entries = localStorage.getItem("user-entries");
+    if (entries){
+        entries = JSON.parse(entries);
+    } else{
+        entries = [];
+    }
+    return entries;
+}
+let userEntries = retrieveEntries();
+
+const displayEntries = () => {
+    const entries = retrieveEntries();
+
+    const tableEntries= entries.map((entry) => {
+        const namecell = `<td class='border px-4 py-2'>${entry.name}</td>`;
+        const emailcell = `<td class='border px-4 py-2'>${entry.email}</td>`;
+        const passwordcell = `<td class='border px-4 py-2'>${entry.password}</td>`;
+        const dobcell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+        const acceptTermscell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
+
+        const row = `<tr>${namecell} ${emailcell} ${passwordcell} ${dobcell} ${acceptTermscell}</tr>`;
+        return row;
 
 
-function validate() {
-    
-    var nameInput = document.getElementById('name');
-    var nameValue = nameInput.value.trim();
-    if (nameValue === '') {
-      alert('Please enter a valid name.');
-      return false;
-    }
-  
-    
-    var emailInput = document.getElementById('email');
-    var emailValue = emailInput.value.trim();
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(emailValue)) {
-      alert('Please enter a valid email address.');
-      return false;
-    }
-  
-    
-    var dobInput = document.getElementById('dob');
-    var dobValue = dobInput.value;
-    var today = new Date();
-    var selectedDate = new Date(dobValue);
-    if (selectedDate >= today) {
-      alert('Please enter a valid date of birth.');
-      return false;
-    }
-    var age = today.getFullYear() - selectedDate.getFullYear();
+    }).join("\n");
 
- 
-  if (age < 18 || age > 55) {
-    alert('Age must be between 18 and 55.');
-    return false;
-  }
+    const table = `<table class="table-auto w-full"><tr>
     
-    var userDetails = {
-      name: nameValue,
-      email: emailValue,
-      dob: dobValue,
+    <th class="px-4 py-2">Name</th>
+    <th class="px-4 py-2">email</th>
+    <th class="px-4 py-2">password</th>
+    <th class="px-4 py-2">dob</th>
+    <th class="px-4 py-2">accepted Terms?</th>
+
+</tr>${tableEntries} </table>`;
+
+let details = document.getElementById("user-entries");
+details.innerHTML= table;
+}
+
+const saveUserForm = (event) => {
+    event.preventDefault();
+    const name= document.getElementById("name").value;
+    const email= document.getElementById("email").value;
+    const password= document.getElementById("password").value;
+    const dobInput= document.getElementById("dob");
+
+    const dob = dobInput.value;
+
+    // Validate date format (YYYY-MM-DD)
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(dob)) {
+        alert("Invalid date format. Please use YYYY-MM-DD.");
+        return;
+    }
+
+    // Validate age between 18 and 55
+    const birthDate = new Date(dob);
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    if (age < 18 || age > 55) {
+        alert("Age must be between 18 and 55.");
+        return;
+    }
+
+
+    const acceptedTermsAndConditions = document.getElementById("acceptTerms").checked;
+
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndConditions,
+
     };
-  
-    var userDetailsList = JSON.parse(localStorage.getItem('userDetailsList')) || [];
-    userDetailsList.push(userDetails);
-    localStorage.setItem('userDetailsList', JSON.stringify(userDetailsList));
-  
-    updateTable();
-  
-    return false; 
-  }
-  
-  function updateTable() {
-    var userDetailsList = JSON.parse(localStorage.getItem('userDetailsList')) || [];
-    var tableContainer = document.getElementById('f');
-  
+    userEntries.push(entry);
 
-    tableContainer.innerHTML = '';
-  
-    if (userDetailsList.length > 0) {
-      // Create a table
-      var table = document.createElement('table');
-      table.setAttribute('border', '1');
-      table.setAttribute('cellpadding', '10');
-  
-    
-      var headerRow = table.insertRow(0);
-      var headers = ['Name', 'Email', 'Date of Birth'];
-      headers.forEach(function (headerText) {
-        var headerCell = document.createElement('th');
-        headerCell.appendChild(document.createTextNode(headerText));
-        headerRow.appendChild(headerCell);
-      });
-  
-   
-      userDetailsList.forEach(function (userDetails) {
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-  
-        cell1.innerHTML = userDetails.name;
-        cell2.innerHTML = userDetails.email;
-        cell3.innerHTML = userDetails.dob;
-      });
-  
-   
-      tableContainer.appendChild(table);
-    }
-  }
+    localStorage.setItem("user-entries", JSON.stringify(userEntries));
+    displayEntries();
 
-  document.addEventListener('DOMContentLoaded', updateTable);
-  
+
+}
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
